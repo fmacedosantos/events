aa7730ec53392174f63eac042c9d2821935486d8
fmacedosantos <felipemacedo986@gmail.com>
2024-08-10 16:02:48 -0300

    onPress sem parâmetro

--- App.tsx
+++ App.tsx
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  async function handlerPress(numero: number) {
-    console.log('Ai ' + numero);
+  async function handlerPress() {
+    console.log('Ai');
   }
   return (
     <View style={styles.container}>
-      <Text onPress={() => handlerPress(1)}>Open up App.tsx to start working on your app!</Text>
+      <Text onPress={handlerPress}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
