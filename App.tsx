6fb2b8ab31c61c2b436735fa797f12007eb8de76
fmacedosantos <felipemacedo986@gmail.com>
2024-08-10 16:02:13 -0300

    onPress com parâmetro

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  async function handlerPress(numero: number) {
+    console.log('Ai ' + numero);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text onPress={() => handlerPress(1)}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
